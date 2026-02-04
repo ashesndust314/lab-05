154a21b9fe9609d1ac4c3ebea56f2fa24bb652ea
ashleymkang@gmail.com <ashleymkang@gmail.com>
2026-02-03 19:47:27 -0700

    added delete function

--- ListyCity/app/src/main/java/com/example/lab5_starter/CityDialogFragment.java
+++ ListyCity/app/src/main/java/com/example/lab5_starter/CityDialogFragment.java
@@ -17,6 +17,7 @@ public class CityDialogFragment extends DialogFragment {
     interface CityDialogListener {
         void updateCity(City city, String title, String year);
         void addCity(City city);
+        void deleteCity(City city);
     }
     private CityDialogListener listener;
 
@@ -51,16 +52,20 @@ public class CityDialogFragment extends DialogFragment {
         Bundle bundle = getArguments();
         City city;
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         if (Objects.equals(tag, "City Details") && bundle != null){
             city = (City) bundle.getSerializable("City");
             assert city != null;
             editMovieName.setText(city.getName());
             editMovieYear.setText(city.getProvince());
+            builder.setNeutralButton("Delete", ((dialogInterface, i) -> {
+                listener.deleteCity(city);
+            }));
         }
         else {
             city = null;}
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
         return builder
                 .setView(view)
                 .setTitle("City Details")
